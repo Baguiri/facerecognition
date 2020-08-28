712fe0ce323b7bade2c6083fc7012e349cad6d4b
Baguiri <baguiri9473@gmail.com>
2020-08-28 22:37:11 +0300

    Updates

---
+++ precache-manifest.2cd0338ded4c4cde89aa2776ee52d614.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f685adb941cc254c5554e4322dee51bc",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "8b53e7999a44331ad6b5",
+    "url": "/facerecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5bb4373babeb81e3282e",
+    "url": "/facerecognition/static/css/main.64547d4b.chunk.css"
+  },
+  {
+    "revision": "8b53e7999a44331ad6b5",
+    "url": "/facerecognition/static/js/2.7f9219e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognition/static/js/2.7f9219e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bb4373babeb81e3282e",
+    "url": "/facerecognition/static/js/main.021cc9f5.chunk.js"
+  },
+  {
+    "revision": "e457364e089452e68aa4",
+    "url": "/facerecognition/static/js/runtime-main.b0d2466f.js"
+  }
+]);
